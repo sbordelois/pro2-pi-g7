f1bd159b567ed225a7ab7e40b24a74a1656f6c5a
DelfinaPinasco <pinascod@udesa.edu.ar>
2023-04-08 11:39:41 -0300

    cambios en data e index

--- Proyecto-Integrador/data/data.js
+++ Proyecto-Integrador/data/data.js
@@ -12,15 +12,92 @@ const data = {
                 }
             ]
         },
-        {}
-    ],
-    users: {
+        
+        {
+            nombre:"Vans LX ",
+            imgUrl:"/images/products/vans-lx.png",
+            precio: "13usd",
+            descripcion:"Descripción corta del producto: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque",
+            comments: [
+                {
+                    usuario: "Pepe",
+                    Comentario: "Que buenas zapatillas"
+                }
+            ]   
 
-    },
-    comments: [
-        {},
-        {}
-    ]
-}
+        }, 
 
+        {
+            nombre:"Nike Air Max ",
+            imgUrl:"/images/products/nike-airmax.png",
+            precio: "20usd",
+            descripcion:"Descripción corta del producto: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque",
+            comments: [
+                {
+                    usuario: "Pepe",
+                    Comentario: "Que buenas zapatillas"
+                }
+            ]
+        }, 
+        {
+            nombre:"Converse CDG ",
+            imgUrl:"/images/products/converse-cdg.png",
+            precio: "22usd",
+            descripcion:"Descripción corta del producto: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque",
+            comments: [
+                {
+                    usuario: "Pepe",
+                    Comentario: "Que buenas zapatillas"
+                }
+            ]
+        },
+        {
+            nombre:"Nike Blazer ",
+            imgUrl:"/images/products/nike-blazer.png",
+            precio: "15usd",
+            descripcion:"Descripción corta del producto: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque",
+            comments: [
+                {
+                    usuario: "Pepe",
+                    Comentario: "Que buenas zapatillas"
+                }
+            ]
+        },
+        {
+            nombre:"Nike Air Jodan ",
+            imgUrl:"/images/products/air-jordan.png",
+            precio: "15usd",
+            descripcion:"Descripción corta del producto: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque",
+            comments: [
+                {
+                    usuario: "Pepe",
+                    Comentario: "Que buenas zapatillas"
+                }
+            ]
+        },
+        {
+            nombre:"New Balance 2002r ",
+            imgUrl:"/images/products/new-balance.png",
+            precio: "14usd",
+            descripcion:"Descripción corta del producto: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque",
+            comments: [
+                {
+                    usuario: "Pepe",
+                    Comentario: "Que buenas zapatillas"
+                }
+            ]
+        },
+        {
+            nombre:"Onitsuka Tiger Mexico 66 ",
+            imgUrl:"/images/products/tiger.png",
+            precio: "12usd",
+            descripcion:"Descripción corta del producto: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque",
+            comments: [
+                {
+                    usuario: "Pepe",
+                    Comentario: "Que buenas zapatillas"
+                }
+            ]
+        }
+    ]}
 //aca falta agregar la info para todos los otros ejemplos que usamos
