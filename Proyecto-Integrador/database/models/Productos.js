b46492977e11c13d36c00e0ebcc670d1237e9b8c
LucasBachrach <lbachrach@udesa.edu.ar>
2023-06-11 17:35:45 -0300

    creacion de modelo de productos

---
+++ Proyecto-Integrador/database/models/Productos.js
@@ -0,0 +1,29 @@
+exports.module = function (sequelize, DataTypes){
+    let alias = "productos"
+    let columnas = {
+        id: {
+            types: dataTypes.INTEGER,
+            PrimaryKey: true
+        },
+        usuario_id: {
+            types: dataTypes.INTEGER,
+        },
+        nombre: {
+            types: dataTypes.STRING
+        },
+        descripcion: {
+            types: dataTypes.STRING
+        }
+
+    }
+
+    let config = {
+        TableName: "productos",
+        timestamps: false
+    }
+
+
+
+    const Productos = sequelize.define(alias, columnas, config)
+    return Productos
+}
