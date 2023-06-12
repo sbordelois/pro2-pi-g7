ea2fb713f619b560ef95aca7fa622ecb1d904d93
DelfinaPinasco <pinascod@udesa.edu.ar>
2023-06-12 14:22:45 -0300

    cambio en models

--- Proyecto-Integrador/database/models/Producto.js
+++ Proyecto-Integrador/database/models/Producto.js
@@ -13,6 +13,9 @@ module.exports = function (sequelize, DataTypes){
         },
         descripcion: {
             types: dataTypes.STRING
+        },
+        img: {
+            types: dataTypes.STRING
         }
 
     }
